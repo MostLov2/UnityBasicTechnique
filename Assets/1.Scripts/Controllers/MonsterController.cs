cc645eb09616359f5b7b3563b6b415210b5f89d5
MostLov2 <93458597+MostLov2@users.noreply.github.com>
2024-07-25 21:40:44 +0900

    MonsterAI
    몬스터 AI 추가

---
+++ Assets/1.Scripts/Controllers/MonsterController.cs
@@ -0,0 +1,115 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MonsterController : BaseController
+{
+    Stat _stat;
+
+    [SerializeField]
+    float _scanRange = 10;
+
+    [SerializeField]
+    float _attackRange = 2;
+    public override void Init()
+    {
+        WorldObjectType = Define.WorldObject.Monster;
+        _stat = gameObject.GetComponent<Stat>();
+
+        if(gameObject.GetComponent<UI_HPBar>() == null)
+            Managers.UI.MakeWorldSpaceUI<UI_HPBar>(transform);
+    }
+
+    protected override void UpdateIdle()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player == null)
+            return;
+
+        float distance = (player.transform.position - transform.position).magnitude;
+        if (distance <= _scanRange)
+        {
+            _lockTarget = player;
+            State = Define.State.Moving;
+            return;
+        }
+
+    }
+    protected override void UpdateMoving()
+    {
+        // 몬스터가 내 사정거리보다 가까우면 공격
+        if (_lockTarget != null)
+        {
+            _destPos = _lockTarget.transform.position;
+            float distance = (_destPos - transform.position).magnitude;
+            if (distance <= _attackRange)
+            {
+                NavMeshAgent nma = gameObject.GetOrAddComponent<NavMeshAgent>();
+                nma.SetDestination(transform.position);
+                State = Define.State.Skill;
+                return;
+            }
+        }
+
+        // 이동
+        Vector3 dir = _destPos - transform.position;
+        if (dir.magnitude < 0.1f)
+        {
+            State = Define.State.Idle;
+        }
+        else
+        {
+            NavMeshAgent nma = gameObject.GetOrAddComponent<NavMeshAgent>();
+            nma.SetDestination(_destPos);
+            nma.speed = _stat.MoveSpeed;
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 20 * Time.deltaTime);
+        }
+    }
+    protected override void UpdateSkill()
+    {
+        if (_lockTarget != null)
+        {
+            Vector3 dir = _lockTarget.transform.position - transform.position;
+            Quaternion quat = Quaternion.LookRotation(dir);
+            transform.rotation = Quaternion.Lerp(transform.rotation, quat, 20 * Time.deltaTime);
+        }
+    }
+    protected override void UpdateDie()
+    {
+        Debug.Log("Monster UpdateDie");
+    }
+    void OnHitEvent()
+    {
+        if (_lockTarget != null)
+        {
+            Stat targetStat = _lockTarget.GetComponent<Stat>();
+            int damage = Mathf.Max(0, _stat.Attack - targetStat.Defense);
+            targetStat.Hp -= damage;
+
+
+            if (targetStat.Hp <= 0)
+            {
+                GameObject.Destroy(targetStat.gameObject);
+            }
+
+            if (targetStat.Hp > 0)
+            {
+                float distance = (_lockTarget.transform.position - transform.position).magnitude;
+                if (distance >= _attackRange)
+                    State = Define.State.Skill;
+                else
+                    State = Define.State.Moving;
+            }
+            else
+            {
+                State = Define.State.Idle;
+
+            }
+
+        }
+        else
+        {
+            State = Define.State.Idle;
+        }
+    }
+}
