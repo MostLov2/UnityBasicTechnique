d3933273668f2bb04ca3a8b4a09af086cef5563d
MostLov2 <93458597+MostLov2@users.noreply.github.com>
2024-07-25 22:37:19 +0900

    LevelUp

--- Assets/1.Scripts/Data/PlayerStat.cs
+++ Assets/1.Scripts/Data/PlayerStat.cs
@@ -1,3 +1,5 @@
+using Data;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerStat : Stat
@@ -7,17 +9,55 @@ public class PlayerStat : Stat
     [SerializeField]
     int _gold;
 
-    public int Exp { get { return _exp; } set { _exp = value; } }
+    public int Exp 
+    {
+        get { return _exp; } 
+        set 
+        { 
+            _exp = value;
+            int level = Level;
+            while (true)
+            {
+                Data.Stat stat;
+                if(Managers.Data.StatDict.TryGetValue( level + 1, out stat) == false)
+                    break;
+                if (_exp < stat.totalExp)
+                    break;
+                level++;
+            }
+            if (level != Level)
+            {
+                Debug.Log("Level Up!!!");
+                Level = level;
+                SetStat(Level);
+            }
+        } 
+    }
     public int Gold { get { return _gold; } set { _gold = value; } }
     private void Start()
     {
+        Dictionary<int, Data.Stat> dict = Managers.Data.StatDict;
+
+        Data.Stat stat = dict[1];
         _level = 1;
-        _hp = 100;
-        _maxHp = 100;
-        _attack = 10;
+        SetStat(_level);
+        _exp = 0;
         _defense = 5;
         _moveSpeed = 5.0f;
-        _exp = 0;
         _gold = 0;
+    }
+    public void SetStat(int level)
+    {
+        Dictionary<int, Data.Stat> dict = Managers.Data.StatDict;
+
+        Data.Stat stat = dict[level];
+        _hp = stat.maxHp;
+        _maxHp = stat.maxHp;
+        _attack = stat.attack;
+
+    }
+    protected override void OnDead(Stat attacker)
+    {
+        Debug.Log("PlayerDead");
     }
 }
--- Assets/1.Scripts/Managers/Contents/GameMnaager.cs
+++ Assets/1.Scripts/Managers/Contents/GameMnaager.cs
@@ -8,6 +8,7 @@ public class GameMnaager
     // int <-> GameObject
     HashSet<GameObject> _monster = new HashSet<GameObject>();
 
+    public GameObject GetPlayer() { return _player; }   
     public GameObject Spawn(Define.WorldObject type, string path, Transform parent = null)
     {
         GameObject go = Managers.Resource.Instantiate(path, parent);
